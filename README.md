f117a22e00f497cb3834ad085e5821c9e2487d50
Autarc <Autarc@gmail.com>
2013-03-27 03:49:36 +0100

    update 0.1.3 (messageQueue)

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ r2d2
 ====
 
 A websocket wrapper, to ease the human-machine interaction.
+
+__Latest Release: 0.1.3__
+
+
+## API
+
+var socket = new vwc.Socket( 'serverAddress:port', socketConfiguration )
--- socket.js
+++ socket.js
@@ -1,4 +1,4 @@
-// Namespace Handling
+// Temporary Namespace Handling
 if ( !window.vwc ) window.vwc = {};
 
 /**
@@ -9,6 +9,7 @@ if ( !window.vwc ) window.vwc = {};
  *
  *  - info
  *  - .on()
+ *  - .off()
  *  - .send()
  *  - .reconnect()
  */
@@ -19,7 +20,8 @@ vwc.Socket = (function(){
 
 	var config = {
 
-			retryTimer:	5000
+			retryTimer	: 5000,
+			encode		: null
 		},
 
 		WEBSOCKET_STATES = {
@@ -34,101 +36,27 @@ vwc.Socket = (function(){
 	/**
 	 *  vwc.Socket - Wrapper Constructor
 	 *
-	 *  @param { Object }	params		- configuration for this socket
+	 *  @param { Object }	server		- server address / information for this socket
+	 *  @param { Object }	options		- additional configurations for the connection
 	 */
-	var Socket = function ( params ) {
+	var Socket = function ( server, options ) {
 
-		this.config = params;
+		this.server = ( typeof server === 'string' ) ?  { server: server } : server;
 
-		this.info = {
+		for ( var keys in options ) config[keys] = options[keys];
 
-			duration	: null,		// Dauer der Verbindung
-			lastPing	: null,		//
-			latency		: null,		//
-			bandwidth	: null,		//
-			extensions	: null,		// gzip ?
-			reconnected	: false		// list of reconnections
-		};
+		this.config			= config;
+
+		this.id				= null;
+
+		this.last			= null;
+
+		this.messageQueue	= [];
+
+		this.info			= prepareInfo();
 
 		connect.call( this );
 	};
-
-
-	/**
-	 *  Creating the socket for connection
-	 */
-	function connect() {
-
-		var config = this.config,
-			socket;
-
-		try {
-
-			if ( config.server ) {
-
-				socket = new WebSocket( 'ws://' + config.server );
-
-			} else {
-
-				socket = new WebSocket( 'ws://' + config.url + ':' + config.port,
-											config.protocol || null );
-			}
-
-		} catch ( e ) {
-
-			console.log( '[error] - ' + e );
-
-			// if ( config.autoReconnect )  this.reconnect();
-		}
-
-		this.socket = socket;
-
-		init.call( this, config.autoReconnect );
-	}
-
-
-	/**
-	 *  [init description]
-	 *  @param  {[type]} autoReconnect [description]
-	 *  @return {[type]}               [description]
-	 */
-	function init ( autoReconnect ) {
-
-		var socket	= this.socket,
-			events	= [ 'open', 'message', 'close', 'error' ],
-			handler = this.handler || {};	// reconnect
-
-		for ( var i = 0, l = events.length; i < l; i++ ) {
-
-			if ( !this.handler ) handler[ events[i] ] = [];
-			socket.addEventListener( events[i],	handle );
-		}
-
-		if ( autoReconnect ) {
-
-			handler.close.push( this.reconnect.bind( this ) );
-		}
-
-		if ( !this.handler ) {
-
-			handler.reconnect = [];
-			this.handler = handler;
-		}
-
-		function handle ( e ) {
-
-			// console.log('[' + e.type + ']');
-			var channel = handler[ e.type ];
-
-			if ( channel.length ) {
-
-				for ( var i = 0, l = channel.length; i < l; i++ ) {
-
-					channel[i]( e );
-				}
-			}
-		}
-	}
 
 
 	/**
@@ -146,41 +74,227 @@ vwc.Socket = (function(){
 
 
 	/**
+	 *  [off description]
+	 *  @param  {[type]}   topic    [description]
+	 *  @param  {Function} callback [description]
+	 *  @return {[type]}            [description]
+	 */
+	Socket.prototype.off = function ( topic, callback ) {
+
+		var listeners = this.handler[ topic ];
+
+		if ( !listeners ) return;
+
+		if ( !callback ) {
+
+			listeners.length = 0;
+
+		} else {
+
+			var length = listeners.length;
+
+			while ( length-- ) {
+
+				console.log(listeners[ length ] === callback);
+
+
+				if ( listeners[ length ] === callback ) {
+
+					listeners.splice( length, 1 ); break;
+				}
+			}
+		}
+	};
+
+
+	/**
 	 *  [reconnect description]
 	 *  @param  {[type]} e [description]
 	 *  @return {[type]}   [description]
 	 */
 	Socket.prototype.reconnect = function ( e ) {
 
-		if ( this.reconnectID ) return;
+		if ( this.reconnectID ) return;	// can't call reconnect as it still is reconnecting
 
-		// connection still there - manual reconnection
+		// connection still there - close existing one
 		if ( this.socket.readyState !== WEBSOCKET_STATES['closed']  ) {
 
 			this.socket.close();
 		}
 
-		this.reconnectID = setInterval( reconnect.bind(this),	this.config.retryTimer ||
-																config.retryTimer );
+		this.reconnectID = setInterval( reconnect.bind(this), this.config.retryTimer );
 	};
 
 
 	/**
-	 *  [reconnect description]
+	 *  [send description]
+	 *  @param  {[type]} msg [description]
+	 *  @return {[type]}     [description]
+	 */
+	Socket.prototype.send = function ( msg ) {
+
+		var encoder	= this.config.encode || JSON.stringify;
+
+		msg = encoder( msg );
+
+		this.messageQueue.push( msg );
+
+		if ( this.socket.readyState !== WEBSOCKET_STATES['open'] ) return;
+
+		transfer.call(this);
+	};
+
+
+
+	/**
+	 *  [prepareInfo description]
 	 *  @return {[type]} [description]
 	 */
-	function reconnect(){
+	function prepareInfo(){
 
-		if (	this.socket.readyState !== WEBSOCKET_STATES['connecting'] &&
-				this.socket.readyState !== WEBSOCKET_STATES['open']				) {
+		var info = {
 
-			// console.log('[ try reconnection ]');
-			connect.call( this, this.config );
+			lastPing	: null,
+			latency		: null,
+			bandwidth	: null,
+			extensions	: null,
 
-		} else {
+			reconnected	: false
+		};
 
-			clearInterval( this.reconnectID );
-			delete this.reconnectID;
+		// duration of the connection
+		Object.defineProperty( info, 'duration', {
+
+			get: function(){
+
+				return Date.now() - this._start;
+			}
+		});
+
+		return info;
+	}
+
+	/**
+	 *  [connect description]
+	 *  Creating the socket for connection
+	 *  @return {[type]} [description]
+	 */
+	function connect() {
+
+		var server = this.server,
+			socket;
+
+		try {
+
+			if ( server.server ) {
+
+				socket = new WebSocket( 'ws://' + server.server );
+
+			} else {
+
+				socket = new WebSocket( 'ws://' + server.url + ':' + server.port, server.protocol || null );
+			}
+
+		} catch ( e ) {
+
+			console.log( '[error] - ' + e );
+
+			return;
+		}
+
+		this.socket = socket;
+
+		init.call( this, this.config.autoReconnect );
+	}
+
+
+	/**
+	 *  [init description]
+	 *  @param  {[type]} autoReconnect [description]
+	 *  @return {[type]}               [description]
+	 */
+	function init ( autoReconnect ) {
+
+		var socket	= this.socket,
+			events	= [ 'open', 'message', 'close', 'error' ],
+			handler = this.handler || {};						// ~ reconnect
+
+		for ( var i = 0, l = events.length; i < l; i++ ) {
+
+			if ( !handler[ events[i] ] ) handler[ events[i] ] = [];
+			socket.addEventListener( events[i],	handle.bind(this) );
+		}
+
+		if ( this.handler ) {									// ~ reconnect
+
+			if ( this.info.reconnected ) return;
+
+			this.info.reconnected = true;
+
+			handler.reconnect.push(function(){
+
+				this.info._start = Date.now();
+
+				transfer.call(this);
+
+			}.bind(this) );
+
+			handler.open = handler.reconnect;
+
+			return;
+		}
+
+
+		if ( autoReconnect ) {
+
+			handler.open.push( function(){
+
+				this.info._start = Date.now();
+
+				handler.close.push( this.reconnect.bind( this ) );
+
+			}.bind(this));
+		}
+
+		handler.reconnect = [];
+		this.handler = handler;
+
+
+		function handle ( e ) {
+
+			if (	e.type	=== this.last &&
+					e.type	=== 'close'			) return;	// prevent multiple firing
+
+			this.last = e.type;
+
+			var channel = handler[ e.type ];
+
+			if ( channel.length ) {
+
+				for ( var i = 0, l = channel.length; i < l; i++ ) {
+
+					channel[i]( e );
+				}
+			}
+		}
+	}
+
+
+	/**
+	 *  [emit description]
+	 *  @param  {[type]} topic [description]
+	 *  @param  {[type]} data  [description]
+	 *  @return {[type]}       [description]
+	 */
+	function emit ( topic, data ) {
+
+		var listeners = this.handler[ topic ],
+
+			length = listeners.length;
+
+		while ( length-- ) {
+
+			listeners[length]( data );
 		}
 	}
 
@@ -200,7 +314,9 @@ vwc.Socket = (function(){
 
 				try {
 
-					msg = JSON.parse(msg);
+					var decoder = this.config.decode ||	JSON.parse;
+
+					msg = decoder( msg );
 
 				} catch ( err ) {
 
@@ -218,27 +334,38 @@ vwc.Socket = (function(){
 
 
 	/**
-	 *  [send description]
-	 *  @param  {[type]} msg [description]
-	 *  @return {[type]}     [description]
+	 *  [transfer description]
+	 *  @return {[type]} [description]
 	 */
-	Socket.prototype.send = function ( msg ) {
+	function transfer(){
 
-		msg = JSON.stringify(msg);
+		var messageQueue	= this.messageQueue,
+			socket			= this.socket;
 
-		this.socket.send( msg );
-	};
+		while ( messageQueue.length ) socket.send( messageQueue.shift() );
+	}
+
+
+	/**
+	 *  [reconnect description]
+	 *  @return {[type]} [description]
+	 */
+	function reconnect(){
+
+		if (	this.socket.readyState !== WEBSOCKET_STATES['connecting'] &&
+				this.socket.readyState !== WEBSOCKET_STATES['open']				) {
+
+			connect.call( this );
+
+		} else {
+
+			clearInterval( this.reconnectID );
+			delete this.reconnectID;
+		}
+	}
 
 
 	return Socket;
 
 })();
 
-
-
-	/**
-	 * Unsubscribe callbacks from a topic.
-
-	 * @param  {String}		topic		- topic of which listeners should be removed
-	 * @param  {Function}	callback	- specific callback which should be removed
-	 */
